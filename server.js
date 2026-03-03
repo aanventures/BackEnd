6acfb3a834399da17257070b7529b933571a6cf0
aanventures <aanventures3@gmail.com>
2026-03-04 03:54:05 +0530

    added new model

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const dotenv = require('dotenv')
 const path = require('path')
+const dns = require('node:dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 const mongoose = require('mongoose')
 
 // Load environment variables from .env in project root
